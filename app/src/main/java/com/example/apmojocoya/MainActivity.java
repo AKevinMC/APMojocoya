db131da0ac8b797f90cefa07616593f97cf43973
A. Kevin Medrano Cuellar <medrano.cuellar.alvarokevin@usfx.bo>
2024-05-10 16:59:18 -0400

    crear proyecto y añadir Apache Poi

---
+++ app/src/main/java/com/example/apmojocoya/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.apmojocoya;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    //verificar si hay permiso de escritura
+        checkPermission();
+
+    // Nombre del archivo xlsx y directorio
+        String FILE_NAME = "test1.xlsx";
+        String directoryPath = Environment.getExternalStorageDirectory() + "/Documents";
+
+    // Crear el directorio (si no existe)
+        File folder = new File(directoryPath);
+    // Log
+        if (folder.mkdirs() || folder.exists()) {
+            Log.d("MainActivity", "Folder created: " + folder.getAbsolutePath());
+        } else {
+            Log.e("MainActivity", "Error creating folder: " + folder.getAbsolutePath());
+        }
+
+    // Crear el archivo xlsx
+        File excelFile = new File(folder , FILE_NAME);
+        Workbook libro = new XSSFWorkbook();
+        Sheet hoja = libro.createSheet("hoja1");
+        hoja.createRow(0).createCell(0).setCellValue("Hola mundo");
+        try {
+            libro.write(new FileOutputStream(excelFile));
+            Log.d("MainActivity", "excelFile created: " + excelFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Error creating excelFile: " + excelFile.getAbsolutePath());
+        }
+
+
+    //cambiar el testo de textView
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Listo!");
+    }
+//Verificar si hay permiso de escritura
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+    }
+}
